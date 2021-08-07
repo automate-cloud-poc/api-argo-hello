d84d8670ff15415d5d90b4e5726eba51b717d8de
Daniel Kanczuk <danielsussa@gmail.com>
2021-08-07 12:29:18 -0300

    config map and secret

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,6 +12,9 @@ import (
 var GitCommit string
 
 func main() {
+	secret1 := os.Getenv("SECRET1")
+	configMap1 := os.Getenv("CONFIGMAP1")
+
 	e := echo.New()
 	e.GET("/health", func(c echo.Context) error {
 		return c.String(http.StatusOK, "pong")
@@ -19,6 +23,12 @@ func main() {
 		log.Println("my hello log")
 		fmt.Println("log backup")
 		return c.String(http.StatusOK, fmt.Sprintf("Hello, from argo! commit %s", GitCommit))
+	})
+	e.GET("/hello/secret", func(c echo.Context) error {
+		return c.String(http.StatusOK, fmt.Sprintf("this is a secret: %s", secret1))
+	})
+	e.GET("/hello/configMap", func(c echo.Context) error {
+		return c.String(http.StatusOK, fmt.Sprintf("this is a config map: %s", configMap1))
 	})
 	e.GET("/argo/hello/auth", func(c echo.Context) error {
 		log.Println("contain auth routes")
